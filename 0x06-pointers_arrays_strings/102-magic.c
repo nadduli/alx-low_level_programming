4805b8300922d1e3e21831cb4c55695e63719924
nadduli <naddulidaniel94@gmail.com>
2021-07-01 04:59:48 -0700

    magic file

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(void)
+/* initializing the main function */
 {
 int n;
 int a[5];
@@ -8,7 +9,7 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-  /*
+/*
    * write your line of code here...
    * Remember:
    * - you are not allowed to use a
