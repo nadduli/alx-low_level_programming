de5a1e01b2f5854252cb3ef24a738ef7f5a8a1a9
nadduli <naddulidaniel94@gmail.com>
2021-07-15 10:13:41 -0700

    program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,30 +1,27 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
 
 /**
- * *array_range - creates an array of integers
- * @min: minimum range of values stored
- * @max: maximum range of values stored and number of elements
- *
- * Return: pointer to the new array
+ * array_range - create anarray of integers (min to max)
+ * @min: min number to start
+ * @max: max number to start
+ * Return: pointer
  */
+
 int *array_range(int min, int max)
 {
 	int *ptr;
-	int i, size;
+	int diff, i;
 
 	if (min > max)
 		return (NULL);
-
-	size = max - min + 1;
-
-	ptr = malloc(sizeof(int) * size);
-
+	diff = max - min;
+	ptr = malloc((diff + 1) * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
-
-	for (i = 0; min <= max; i++)
-		ptr[i] = min++;
-
+	for (i = 0; i <= diff; min++, i++)
+	{
+		ptr[i] = min;
+	}
 	return (ptr);
 }
